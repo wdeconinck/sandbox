1077a6b34dd33fb3e9d2c902e5fc8e63d7c88bd0
Willem Deconinck <willem.deconinck@ecmwf.int>
2018-03-07 09:27:53 +0000

    Play with cache

--- install-mpich.sh
+++ install-mpich.sh
@@ -6,18 +6,15 @@ fi
 if [ -f mpich/lib/libmpich.so ]; then
   echo "libmpich.so found -- nothing to build."
 else
-  if [ -f mpich-3.2/src/mpi/init/mpi_init.h ]; then
-    echo "mpich sources found -- don't need to download"
-  else
-    echo "Downloading mpich source."
-    wget http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
-    tar xfz mpich-3.2.tar.gz
-    rm mpich-3.2.tar.gz
-  fi
-  echo "configuring and building mpich."
+  echo "Downloading mpich source..."
+  rm -rf mpich-3.2
+  wget http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+  tar xfz mpich-3.2.tar.gz
+  rm mpich-3.2.tar.gz
+  echo "Configuring and building mpich..."
   cd mpich-3.2
   ./configure \
-          --prefix=`pwd`/../mpich \
+          --prefix=$(pwd)/../mpich \
           --enable-static=false \
           --enable-alloca=true \
           --disable-long-double \

2a741c74fecd4a29f6b69c038dbc7d4d8f64f292
Willem Deconinck <willem.deconinck@ecmwf.int>
2018-03-08 15:59:43 +0000

    cache logic for fckit

--- play-sed.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Abort on Error
-set -ex
-GITHUB_USER=$1
-REPO_NAME=$2
-REPO_BRANCH=$3
-wget -qO- https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/commits/${REPO_BRANCH} | grep sha | head -1 | sed -n 's/.*"\(.*\)",.*/\1/p'

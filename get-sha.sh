4648e746d4330015b5c58d916851b80b437e8d36
Willem Deconinck <willem.deconinck@ecmwf.int>
2018-03-08 15:51:20 +0000

    use wget

--- get-sha.sh
+++ get-sha.sh
@@ -4,4 +4,4 @@ set -e
 GITHUB_USER=$1
 REPO_NAME=$2
 REPO_BRANCH=$3
-curl -s https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/commits/${REPO_BRANCH} | grep sha | head -1 | sed -n 's/.*"\(.*\)",.*/\1/p'
+wget -qO- https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/commits/${REPO_BRANCH} | grep sha | head -1 | sed -n 's/.*"\(.*\)",.*/\1/p'
--- play-sed.sh
+++ play-sed.sh
@@ -4,7 +4,4 @@ set -ex
 GITHUB_USER=$1
 REPO_NAME=$2
 REPO_BRANCH=$3
-wget -qO- https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/commits/${REPO_BRANCH} | grep sha
-wget -qO- https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/commits/${REPO_BRANCH} | grep sha | head -1
 wget -qO- https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/commits/${REPO_BRANCH} | grep sha | head -1 | sed -n 's/.*"\(.*\)",.*/\1/p'
-wget -qO- https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/commits/${REPO_BRANCH} | grep sha | head -1 | sed -n 's/.*"(.*)",.*/\1/p'

d021fac6f4cd8c1fc8d0db068df32e132fbc41b3
Willem Deconinck <willem.deconinck@ecmwf.int>
2018-03-08 23:02:31 +0000

    use git

--- ci/github-sha.sh
+++ ci/github-sha.sh
@@ -4,11 +4,27 @@ owner=$1
 repo=$2
 branch=$3
 
-git clone --depth 1 -b ${branch} git@github.com:${owner}/${repo} tmp-$repo &> /dev/null
-cd tmp-$repo;
+
+dump_output() {
+   echo " ++ Tailing the last 500 lines of output from $BUILD_OUTPUT"
+   tail -500 $BUILD_OUTPUT  
+}
+error_handler() {
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+BUILD_OUTPUT=$(pwd)/tmp-${repo}.log
+touch $BUILD_OUTPUT
+
+git clone --depth=1 -b ${branch} git@github.com:${owner}/${repo} tmp-${repo} >> $BUILD_OUTPUT 2>&1 
+cd tmp-${repo} >> $BUILD_OUTPUT 2>&1 
 git rev-parse HEAD
 cd ..
-rm -rf tmp-$repo
+rm -rf tmp-${repo}*
 
 #
 # n=0

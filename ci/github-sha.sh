897a67e84dfc5b22cb4fe3477f265267b98e2dfd
Willem Deconinck <willem.deconinck@ecmwf.int>
2018-03-10 16:15:08 +0000

    Fix missing TMPDIR

--- ci/github-sha.sh
+++ ci/github-sha.sh
@@ -4,11 +4,14 @@ owner=$1
 repo=$2
 branch=$3
 
+if [ -z "${TMPDIR}" ]; then
+  TMPDIR=${HOME}/tmp
+fi
 SOURCE_DIR=${TMPDIR}/downloads/check-git-${repo}
 
 dump_output() {
-   echo " ++ Tailing the last 500 lines of output from $BUILD_OUTPUT"
-   tail -500 $BUILD_OUTPUT  
+   echo " ++ Tailing the last 500 lines of output from ${BUILD_OUTPUT}"
+   tail -500 ${BUILD_OUTPUT}
 }
 error_handler() {
   echo ERROR: An error was encountered with the build.
